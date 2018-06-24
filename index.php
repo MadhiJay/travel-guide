59b2e8a57259bd6efe1deeffce956bdc5d8a6403
Erandra Jayasundara <erandrajayasundara@live.com>
2018-06-24 13:18:08 +0530

    test

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+$method=$_SERVER['REQUEST_METHOD'];
+if($method=="POST"){
+    $requestBody=file_get_contents('php://input');
+    $json=json_decode($requestBody);
+    $text=$json-> result->parameters->text;
+    switch ($text) {
+        case 'hi':
+            $speech="Hi,Nice to meet you";
+            break;
+        case 'bye':
+            $speech="bye,Nice to meet you";
+            break;
+        default:
+            $speech="sorry, I didn't get that";
+            break;
+    }
+    $response=new \stdClass();
+    $response->speech="";
+    $response->displayText="";
+    $response->source="webhook";
+    echo json_encode($response);
+}else{
+     echo "Method not allowed";
+}
+
+?>
